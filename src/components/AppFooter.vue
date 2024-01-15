397cf43eccb23aa4705da1440716b510b10ca803
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-01-15 22:35:31 +0100

    AppMain: Jumbotron nested and working on

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,6 +9,6 @@ export default {
 </template>
 
 <style lang="scss">
-@use "../styles/general.scss"
+@use "../styles/general.scss";
 
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,11 +5,33 @@ export default {
 </script>
 
 <template>
-  <h1>Main page</h1>
-
+<!-- Jumbotron -->
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../../public/jumbotron/short-slider-rev-1-img-3.png" class="img-fluid" alt="jumbotron img">
+    </div>
+    <div class="carousel-item">
+      <img src="../../public/jumbotron/short-slider-rev-1-img-1.png" class="img-fluid" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../../public/jumbotron/short-slider-rev-1-img-5.png" class="img-fluid" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 </template>
 
-<style lang="scss">
-@use "../styles/general.scss"
+<style lang="scss" scoped>
+@use "../styles/general.scss";
+
+
 
 </style>
