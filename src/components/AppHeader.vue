e5fdf7238b117fd68afcf7de77797f14abf9e36c
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-16 18:12:07 +0100

    amend: improve AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -32,185 +32,202 @@ export default {
             <!-- dropdown menu -->
             <div class="dropdown">
 
-                <!-- Home -->
-                <button class="btn my_btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    HOME
-                </button>
-                <ul class="dropdown-menu my_dropdown-menu">
-                    <li v-for="(element, i) in store.homeLinks" :key="i">
-                        <a class="dropdown-item" :href="element.href">
-                            {{ element.name }}
-                        </a>
-                    </li>
-                </ul>
+                <div class="container-fluid d-flex">
 
-                <!-- Pages -->
-                <button class="btn my_btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    PAGES
-                </button>
-                <ul class="dropdown-menu my_dropdown-menu">
-                    <li v-for="(element, i) in store.pagesLinks" :key="i">
-                        <a class="dropdown-item" :href="element.href">
-                            {{ element.name }}
-                        </a>
-                    </li>
-                </ul>
+                    <div class="row align-items-center">
 
-                <!-- Portfolio -->
-                <button class="btn my_btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    PORTFOLIO
-                </button>
-                <ul class="dropdown-menu my_dropdown-menu portfolio_dropdown my_dropdown">
-
-                    <div class="row my_row">
-                        <div class="col">
-                            <span>TYPES</span>
-                            <ul>
-                                <li v-for="(element, i) in store.typeLinks" :key="i">
-                                    <a class="dropdown-item" :href="element.href">
-                                        {{ element.name }}
-                                    </a>
-                                </li>
-                            </ul>
-
+                        <!-- Home -->
+                        <div class=" col custom-cursor" data-bs-toggle="dropdown" aria-expanded="false">
+                            HOME
                         </div>
+                        <ul class="dropdown-menu my_dropdown-menu">
+                            <li v-for="(element, i) in store.homeLinks" :key="i">
+                                <a class="dropdown-item" :href="element.href">
+                                    {{ element.name }}
+                                </a>
+                            </li>
+                        </ul>
 
-                        <div class="col">
-                            <span>LAYOUTS</span>
-                            <ul>
-                                <li v-for="(element, i) in store.layoutsLinks" :key="i">
-                                    <a class="dropdown-item" :href="element.href">
-                                        {{ element.name }}
-                                    </a>
-                                </li>
-                            </ul>
-
+                        <!-- Pages -->
+                        <div class="col custom-cursor" data-bs-toggle="dropdown" aria-expanded="false">
+                            PAGES
                         </div>
-                        <div class="col">
-                            <span>HOVER TYPES</span>
-                            <ul>
-                                <li v-for="(element, i) in store.hoverTypesLinks" :key="i">
-                                    <a class="dropdown-item" :href="element.href">
-                                        {{ element.name }}
-                                    </a>
-                                </li>
-                            </ul>
+                        <ul class="dropdown-menu my_dropdown-menu">
+                            <li v-for="(element, i) in store.pagesLinks" :key="i">
+                                <a class="dropdown-item" :href="element.href">
+                                    {{ element.name }}
+                                </a>
+                            </li>
+                        </ul>
 
+                        <!-- Portfolio -->
+                        <div class="col custom-cursor" data-bs-toggle="dropdown" aria-expanded="false">
+                            PORTFOLIO
                         </div>
-                        <div class="col">
-                            <span>SINGLES</span>
-                            <ul>
-                                <li v-for="(element, i) in store.singleLinks" :key="i">
-                                    <a class="dropdown-item" :href="element.href">
-                                        {{ element.name }}
-                                    </a>
-                                </li>
-                            </ul>
+                        <ul class="dropdown-menu my_dropdown-menu portfolio_dropdown my_dropdown">
 
+                            <div class="row my_row">
+                                <div class="col">
+                                    <span>TYPES</span>
+                                    <ul>
+                                        <li v-for="(element, i) in store.typeLinks" :key="i">
+                                            <a class="dropdown-item" :href="element.href">
+                                                {{ element.name }}
+                                            </a>
+                                        </li>
+                                    </ul>
+
+                                </div>
+
+                                <div class="col">
+                                    <span>LAYOUTS</span>
+                                    <ul>
+                                        <li v-for="(element, i) in store.layoutsLinks" :key="i">
+                                            <a class="dropdown-item" :href="element.href">
+                                                {{ element.name }}
+                                            </a>
+                                        </li>
+                                    </ul>
+
+                                </div>
+                                <div class="col">
+                                    <span>HOVER TYPES</span>
+                                    <ul>
+                                        <li v-for="(element, i) in store.hoverTypesLinks" :key="i">
+                                            <a class="dropdown-item" :href="element.href">
+                                                {{ element.name }}
+                                            </a>
+                                        </li>
+                                    </ul>
+
+                                </div>
+                                <div class="col">
+                                    <span>SINGLES</span>
+                                    <ul>
+                                        <li v-for="(element, i) in store.singleLinks" :key="i">
+                                            <a class="dropdown-item" :href="element.href">
+                                                {{ element.name }}
+                                            </a>
+                                        </li>
+                                    </ul>
+
+                                </div>
+                            </div>
+                        </ul>
+
+                        <!-- Blog -->
+                        <div class="col custom-cursor" data-bs-toggle="dropdown" aria-expanded="false">
+                            BLOG
                         </div>
+                        <ul class="dropdown-menu my_dropdown-menu">
+                            <li v-for="(element, i) in store.blogLinks" :key="i">
+                                <a class="dropdown-item" :href="element.href">
+                                    {{ element.name }}
+                                </a>
+                            </li>
+                        </ul>
+
+                        <!-- Shop -->
+                        <div class="col custom-cursor" data-bs-toggle="dropdown" aria-expanded="false">
+                            SHOP
+                        </div>
+                        <ul class="dropdown-menu my_dropdown-menu">
+                            <li v-for="(element, i) in store.shopLinks" :key="i">
+                                <a class="dropdown-item" :href="element.href">
+                                    {{ element.name }}
+                                </a>
+                            </li>
+                        </ul>
+
+                        <!-- Elements -->
+                        <div class="col custom-cursor" data-bs-toggle="dropdown" aria-expanded="false">
+                            ELEMENTS
+                        </div>
+                        <ul class="dropdown-menu my_dropdown-menu portfolio_dropdown">
+
+                            <div class="row my_row">
+                                <div class="col-3">
+                                    <span>TYPES</span>
+                                    <ul>
+                                        <li v-for="(element, i) in store.typeLinks" :key="i">
+                                            <a class="dropdown-item" :href="element.href">
+                                                {{ element.name }}
+                                            </a>
+                                        </li>
+                                    </ul>
+
+                                </div>
+
+                                <div class="col-3">
+                                    <span>LAYOUTS</span>
+                                    <ul>
+                                        <li v-for="(element, i) in store.layoutsLinks" :key="i">
+                                            <a class="dropdown-item" :href="element.href">
+                                                {{ element.name }}
+                                            </a>
+                                        </li>
+                                    </ul>
+
+                                </div>
+                                <div class="col-3">
+                                    <span>HOVER TYPES</span>
+                                    <ul>
+                                        <li v-for="(element, i) in store.hoverTypesLinks" :key="i">
+                                            <a class="dropdown-item" :href="element.href">
+                                                {{ element.name }}
+                                            </a>
+                                        </li>
+                                    </ul>
+
+                                </div>
+                                <div class="col-3">
+                                    <span>SINGLES</span>
+                                    <ul>
+                                        <li v-for="(element, i) in store.singleLinks" :key="i">
+                                            <a class="dropdown-item" :href="element.href">
+                                                {{ element.name }}
+                                            </a>
+                                        </li>
+                                    </ul>
+
+                                </div>
+                            </div>
+                        </ul>
+
                     </div>
-                </ul>
 
-                <!-- Blog -->
-                <button class="btn my_btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    BLOG
-                </button>
-                <ul class="dropdown-menu my_dropdown-menu">
-                    <li v-for="(element, i) in store.blogLinks" :key="i">
-                        <a class="dropdown-item" :href="element.href">
-                            {{ element.name }}
-                        </a>
-                    </li>
-                </ul>
+                    <div class="col">
 
-                <!-- Shop -->
-                <button class="btn my_btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    SHOP
-                </button>
-                <ul class="dropdown-menu my_dropdown-menu">
-                    <li v-for="(element, i) in store.shopLinks" :key="i">
-                        <a class="dropdown-item" :href="element.href">
-                            {{ element.name }}
-                        </a>
-                    </li>
-                </ul>
-
-                <!-- Elements -->
-                <button class="btn my_btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    ELEMENTS
-                </button>
-                <ul class="dropdown-menu my_dropdown-menu portfolio_dropdown my_dropdown">
-
-                    <div class="row my_row">
-                        <div class="col">
-                            <span>TYPES</span>
-                            <ul>
-                                <li v-for="(element, i) in store.typeLinks" :key="i">
-                                    <a class="dropdown-item" :href="element.href">
-                                        {{ element.name }}
-                                    </a>
-                                </li>
-                            </ul>
-
-                        </div>
-
-                        <div class="col">
-                            <span>LAYOUTS</span>
-                            <ul>
-                                <li v-for="(element, i) in store.layoutsLinks" :key="i">
-                                    <a class="dropdown-item" :href="element.href">
-                                        {{ element.name }}
-                                    </a>
-                                </li>
-                            </ul>
-
-                        </div>
-                        <div class="col">
-                            <span>HOVER TYPES</span>
-                            <ul>
-                                <li v-for="(element, i) in store.hoverTypesLinks" :key="i">
-                                    <a class="dropdown-item" :href="element.href">
-                                        {{ element.name }}
-                                    </a>
-                                </li>
-                            </ul>
-
-                        </div>
-                        <div class="col">
-                            <span>SINGLES</span>
-                            <ul>
-                                <li v-for="(element, i) in store.singleLinks" :key="i">
-                                    <a class="dropdown-item" :href="element.href">
-                                        {{ element.name }}
-                                    </a>
-                                </li>
-                            </ul>
-
-                        </div>
                     </div>
-                </ul>
+                    <!-- off canvas menu -->
+                    <i class="fa-solid fa-bars btn btn-primary" style="color: #000000;" data-bs-toggle="offcanvas"
+                        data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"></i>
 
-            </div>
+                    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight"
+                        aria-labelledby="offcanvasRightLabel">
+                        <div class="offcanvas-header">
+                            <img src="../../public/logo-sidearea-1.png" alt="Marèe">
+                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body my_offcanvas_body">
+                            <span>This is Marèe, a colorful new portfolio for presenting your work. If you are a modern
+                                illustrator
+                                or designer, we made Marèe for you</span>
+                            <div id="social_icon">
+                                <i class="fa-brands fa-twitter fa-xl" style="color: #000000;"></i>
+                                <i class="fa-brands fa-pinterest fa-xl" style="color: #000000;"></i>
+                                <i class="fa-brands fa-facebook fa-xl" style="color: #000000;"></i>
+                                <i class="fa-brands fa-linkedin fa-xl" style="color: #000000;"></i>
+                            </div>
 
-            <!-- off canvas menu -->
-            <i class="fa-solid fa-bars btn btn-primary" style="color: #000000;" data-bs-toggle="offcanvas"
-                data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"></i>
+                            <input type="text" placeholder="Enter your keyword...">
 
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-                <div class="offcanvas-header">
-                    <img src="../../public/logo-sidearea-1.png" alt="Marèe">
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+
+                    </div>
+
                 </div>
-                <div class="offcanvas-body my_offcanvas_body">
-                    <span>This is Marèe, a colorful new portfolio for presenting your work. If you are a modern illustrator
-                        or designer, we made Marèe for you</span>
-                    <div id="social_icon">
-                        <i class="fa-brands fa-twitter" style="color: #000000;"></i>
-                        <i class="fa-brands fa-pinterest" style="color: #000000;"></i>
-                        <i class="fa-brands fa-facebook" style="color: #000000;"></i>
-                        <i class="fa-brands fa-linkedin" style="color: #000000;"></i>
-                    </div>
-                </div>
+
             </div>
 
         </nav>
@@ -218,7 +235,7 @@ export default {
     </header>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 header {
     height: 100px;
     display: flex;
@@ -229,17 +246,41 @@ header {
 
 nav {
     display: flex;
+    align-items: center;
+    height: 100px;
+}
+
+.container-fluid {
+    align-items: center;
+}
+
+.col .dropdown {
+    align-items: center;
+    height: 100%;
+    display: flex;
+}
+
+div.custom-cursor {
+    cursor: pointer;
+}
+
+.custom-cursor {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    margin-right: 80px;
+    height: 100px;
 }
 
 .my_dropdown-menu {
-    background-color: #FAF7F2 !important;
-    border: none !important;
+    background-color: #FAF7F2;
+    border: none;
     line-height: 30px;
 }
 
 .portfolio_dropdown {
-    width: 100vw !important;
-    padding: 50px !important;
+    width: 100vw;
+    padding: 50px;
 }
 
 span {
@@ -257,8 +298,12 @@ ul {
 }
 
 .fa-solid {
-    background-color: white !important;
-    border: none !important;
+    background-color: white;
+    border: none;
+}
+
+.fa-solid:hover {
+    background-color: white;
 }
 
 .my_offcanvas_body {
@@ -276,7 +321,16 @@ ul {
     }
 
     i {
-        margin-right: 10px;
+        margin-right: 15px;
     }
+
+    input {
+        border: none;
+        border-bottom: 2px solid #F5D8CA;
+        margin-top: 100px;
+        width: 100%
+    }
+
+    ;
 }
 </style>
