22fb225090b23f8644c1a1a5230c428cfed42652
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-16 09:21:45 +0100

    add new component App6Squares

---
+++ src/components/App6Squares.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'App6Squares'
+}
+</script>
+
+<template>
+    <h1>Sezione con 6 quadrati</h1>
+</template>
+
+<style lang="scss">
+@use "../styles/general.scss"
+</style>
