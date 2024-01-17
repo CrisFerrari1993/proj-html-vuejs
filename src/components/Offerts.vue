91df38a6d90be2e3f387e99347e7bc7b2077f57f
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-17 15:30:46 +0100

    create: Offerts component

---
+++ src/components/Offerts.vue
@@ -0,0 +1,10 @@
+<!-- questo componente è quello dove sulla sinistra c'è una ragazza con i capelli rossi e le cuffie gialle e sulla destra le voci animation, design, lettering -->
+<script>
+export default {
+    name: 'Offerts'
+}
+</script>
+
+<template></template>
+
+<style lang="scss" scoped></style>
