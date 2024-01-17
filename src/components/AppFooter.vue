c77a971ecddb0f0278f409bb77fa1a199c0f1a4d
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-17 08:45:33 +0100

    add: responsive footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -7,20 +7,20 @@ export default {
 <template>
   <footer>
     <div class="container-fluid">
-      <div class="row">
-        <div class="col-12 col-md-6 col-lg-3 text-center my-col mb-3">
+      <div class="row align-items-center">
+        <div class="col-sm-12 col-md-6 col-lg-3 text-center my-col mb-3">
           <img src="../../public/logo-sidearea-1.png" alt="Marèe">
           <span>Let's Get Creative</span>
         </div>
-        <div class="col-12 col-md-6 col-lg-3 text-center my-col mb-3">
+        <div class="col-sm-12 col-md-6 col-lg-3 text-center my-col mb-5">
           <span>maree@qodeinteractive.com</span>
           <span>+44645 321 789</span>
         </div>
-        <div class="col-12 col-md-6 col-lg-3 text-center my-col mb-3">
+        <div class="col-sm-12 col-md-6 col-lg-3 text-center my-col mb-5">
           <span>Avenue d'Auberghem 10</span>
           <span>1040 Brussels, Belgium</span>
         </div>
-        <div class="col-12 col-md-6 col-lg-3 text-center my-col mb-3">
+        <div class="col-sm-12 col-md-6 col-lg-3 text-center my-col mb-5">
           <span>Stay in touch with us</span>
           <div id="social_icon">
             <i class="fa-brands fa-twitter" style="color: #000000;"></i>
@@ -39,7 +39,8 @@ export default {
 <style lang="scss" scoped>
 footer {
   background-color: #BFE1CF;
-  height: 20vh;
+  padding-top: 50px;
+  padding-bottom: 40px;
   display: flex;
   align-items: center;
 }
