5f9529dbdcbf6b3b3cb5e3e88a905fcd5e2c0e4c
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-01-17 10:45:43 +0100

    Router: add OurTeam

--- src/router.js
+++ src/router.js
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Home from './pages/Home.vue';
 import About from './pages/About.vue';
-import NotFound from './pages/NotFound.vue'
+import NotFound from './pages/NotFound.vue';
+import OurTeam from './pages/OurTeam.vue';
 
 const router = createRouter ({
     history: createWebHistory(),
@@ -22,6 +23,11 @@ const router = createRouter ({
             name: 'NotFound',
             component: NotFound
         },
+        {
+            path: '/ourteam',
+            name: 'ourteam',
+            component: OurTeam
+        }
     ]
 })
 export { router };
