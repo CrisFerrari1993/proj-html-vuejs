873f0d706343aa9aa01b1ef2b61ecc8f5e0576d7
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-17 15:10:14 +0100

    add: jumbotron

--- src/pages/WhatWeOffer.vue
+++ src/pages/WhatWeOffer.vue
@@ -7,8 +7,34 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <div class="jumbotron">
+        <h1 class="big_font">
+            What we <strong><em>offer</em></strong>
+        </h1>
+    </div>
+</template>
 
 <style lang="scss">
 @use "../styles/general.scss";
+
+.jumbotron {
+    width: 100%;
+    height: 300px;
+    // da modificare con img di background
+    background-color: #FAF7F1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+h1.big_font {
+    font-size: 4rem;
+    font-weight: 300;
+
+    em {
+        font-family: Georgia, 'Times New Roman', Times, serif;
+        font-weight: 700;
+    }
+}
 </style>
