3e436aec6af88a8a14196fc15c0b718fed4aa4ae
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-17 21:53:02 +0100

    fix

--- src/store.js
+++ src/store.js
@@ -21,18 +21,7 @@ export const store = reactive({
         {
             name: 'Pages',
             links: [
-<<<<<<< HEAD
-                { name: 'About Us', href: '#'},
-                { name: 'About Me', href: '#'},
-                { name: 'What we Offer', href: '/whatweoffer'},
-                { name: 'Working Process', href: '#'},
-                { name: 'Our Team', href: '/ourteam'},
-                { name: 'Pricing Plans', href: '#'},
-                { name: 'Contact Us', href: '#'},
-                { name: 'Cooming Soon', href: '#'},
-                { name: '404 Error Page', href: '/error'}
-            ] 
-=======
+
                 { name: 'About Us', href: '#' },
                 { name: 'About Me', href: '#' },
                 { name: 'What we Offer', href: '/whatweoffer' },
@@ -41,9 +30,9 @@ export const store = reactive({
                 { name: 'Pricing Plans', href: '#' },
                 { name: 'Contact Us', href: '#' },
                 { name: 'Cooming Soon', href: '#' },
-                { name: '404 Error Page', href: '#' }
+                { name: '404 Error Page', href: '/error' }
             ]
->>>>>>> f8e7f22 (tentativo di fare la navbar)
+
         },
         {
             name: 'Portfolio',
@@ -205,9 +194,6 @@ export const store = reactive({
             ]
         },
     ],
-<<<<<<< HEAD
-});
-=======
 
     // home links
     homeLinks: [
@@ -239,69 +225,67 @@ export const store = reactive({
     // strat portfolio section links
 
     // type links
-    typeLinks: [
-        { name: 'Standard', href: '#' },
-        { name: 'Gallery', href: '#' },
-        { name: 'Gallery Joined', href: '#' },
-        { name: 'Masonary', href: '#' },
-        { name: 'Pinterest', href: '#' },
-        { name: 'Pricing Plans', href: '#' },
-        { name: 'Scattered', href: '#' },
-        { name: 'Slider', href: '#' },
-    ],
+    // typeLinks: [
+    //     { name: 'Standard', href: '#' },
+    //     { name: 'Gallery', href: '#' },
+    //     { name: 'Gallery Joined', href: '#' },
+    //     { name: 'Masonary', href: '#' },
+    //     { name: 'Pinterest', href: '#' },
+    //     { name: 'Pricing Plans', href: '#' },
+    //     { name: 'Scattered', href: '#' },
+    //     { name: 'Slider', href: '#' },
+    // ],
 
     // layouts links
-    layoutsLinks: [
-        { name: 'Two Columns', href: '#' },
-        { name: 'Three Columns', href: '#' },
-        { name: 'Three Columns Wide', href: '#' },
-        { name: 'Four Columns', href: '#' },
-        { name: 'Five Columns', href: '#' },
-        { name: 'Five Columns Wide', href: '#' },
-        { name: 'Six Columns Wide', href: '#' },
-    ],
+    // layoutsLinks: [
+    //     { name: 'Two Columns', href: '#' },
+    //     { name: 'Three Columns', href: '#' },
+    //     { name: 'Three Columns Wide', href: '#' },
+    //     { name: 'Four Columns', href: '#' },
+    //     { name: 'Five Columns', href: '#' },
+    //     { name: 'Five Columns Wide', href: '#' },
+    //     { name: 'Six Columns Wide', href: '#' },
+    // ],
 
     //hover types links
-    hoverTypesLinks: [
-        { name: 'Slide in', href: '#' },
-        { name: 'Floot', href: '#' },
-        { name: 'Overlay', href: '#' },
-        { name: 'Zoom', href: '#' },
-        { name: 'Shader', href: '#' },
-        { name: 'Clip Image', href: '#' },
-    ],
+    // hoverTypesLinks: [
+    //     { name: 'Slide in', href: '#' },
+    //     { name: 'Floot', href: '#' },
+    //     { name: 'Overlay', href: '#' },
+    //     { name: 'Zoom', href: '#' },
+    //     { name: 'Shader', href: '#' },
+    //     { name: 'Clip Image', href: '#' },
+    // ],
 
     // singles links
-    singleLinks: [
-        { name: 'Split Screen', href: '#' },
-        { name: 'Full Width Slider', href: '#' },
-        { name: 'Wide Slider', href: '#' },
-        { name: 'Four Columns', href: '#' },
-        { name: 'Small Slider', href: '#' },
-        { name: 'Big Images', href: '#' },
-        { name: 'Small Images', href: '#' },
-        { name: 'Big Gallery', href: '#' },
-        { name: 'Small Gallery', href: '#' },
-        { name: 'Masonry', href: '#' },
-    ],
+    // singleLinks: [
+    //     { name: 'Split Screen', href: '#' },
+    //     { name: 'Full Width Slider', href: '#' },
+    //     { name: 'Wide Slider', href: '#' },
+    //     { name: 'Four Columns', href: '#' },
+    //     { name: 'Small Slider', href: '#' },
+    //     { name: 'Big Images', href: '#' },
+    //     { name: 'Small Images', href: '#' },
+    //     { name: 'Big Gallery', href: '#' },
+    //     { name: 'Small Gallery', href: '#' },
+    //     { name: 'Masonry', href: '#' },
+    // ],
 
     // end portfolio section
 
     // blog links
-    blogLinks: [
-        { name: 'Right Sidebar', href: '#' },
-        { name: 'Left Sidebar', href: '#' },
-        { name: 'Centered', href: '#' },
-        { name: 'Post Types', href: '#' },
-    ],
+    // blogLinks: [
+    //     { name: 'Right Sidebar', href: '#' },
+    //     { name: 'Left Sidebar', href: '#' },
+    //     { name: 'Centered', href: '#' },
+    //     { name: 'Post Types', href: '#' },
+    // ],
 
     // shop links
-    shopLinks: [
-        { name: 'Product List', href: '#' },
-        { name: 'Product Single', href: '#' },
-        { name: 'Shop Layouts', href: '#' },
-        { name: 'Shop Pages', href: '#' },
-    ]
-
+    // shopLinks: [
+    //     { name: 'Product List', href: '#' },
+    //     { name: 'Product Single', href: '#' },
+    //     { name: 'Shop Layouts', href: '#' },
+    //     { name: 'Shop Pages', href: '#' },
+    // ]
 })
->>>>>>> f8e7f22 (tentativo di fare la navbar)
