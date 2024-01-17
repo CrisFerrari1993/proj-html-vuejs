c2f98e9ce9d5c0c09b912ecf04e469d8c35480f7
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-01-17 10:45:28 +0100

    App: Removed appmain and filled in Home

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
 
 
 export default {
     components: {
         AppHeader,
-        AppMain,
         AppFooter
     }
 }
@@ -16,7 +14,6 @@ export default {
 <template>
   <AppHeader />
   <main>
-        <AppMain/>
         <router-view></router-view>
   </main>
   <AppFooter/>
