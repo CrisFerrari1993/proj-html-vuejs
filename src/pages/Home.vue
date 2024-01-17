f94859c5a52e8f850834d7b5e26ac9576a7af8c3
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-01-17 10:47:39 +0100

    Home: components including

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,32 @@
 <script>
+import App6Squares from '../components/App6Squares.vue';
+import AppContacts from '../components/AppContacts.vue';
+import AppJumbotron from '../components/AppJumbotron.vue';
+import AppWorkingProcess from '../components/AppWorkingProcess.vue';
+import AppCalendar from '../components/AppCalendar.vue';
 export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+    App6Squares,
+    AppContacts,
+    AppJumbotron,
+    AppWorkingProcess,
+    AppCalendar
+  }
 }
 </script>
 
 <template>
-  <h1>Home</h1>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore fuga eum eos iusto eius nemo vel harum id, illo omnis magni ullam dolorem odit impedit officia, libero ducimus atque voluptatum?</p>
+  <!-- Jumbotron -->
+  <AppJumbotron />
+  <!-- Sezione con 6 quadrati -->
+  <App6Squares />
+  <!-- Working Progress -->
+  <AppWorkingProcess />
+  <!-- Sezione contatti -->
+  <AppContacts />
+  <!-- Sezione calendario -->
+  <AppCalendar />
 </template>
 
 <style lang="scss">
