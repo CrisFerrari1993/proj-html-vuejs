a64551fe5b028c6d8086285153dffdccc09e5b03
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-17 15:49:49 +0100

    add: content in Offerts components

--- src/components/Offerts.vue
+++ src/components/Offerts.vue
@@ -5,6 +5,26 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <div class="container my_container">
+        <div class="row">
+            <div class="col-12  col-md-6">
+                <img src="https://maree.qodeinteractive.com/wp-content/uploads/2019/05/what-we-offer-img-1.jpg" alt="">
+            </div>
+            <div class="col-12  col-md-6 d-flex align-items-center justify-content-center">
+                <span>Bootstrap element</span>
+            </div>
+        </div>
+    </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+img {
+    width: 100%;
+}
+
+.my_container {
+    margin-top: 130px;
+    margin-bottom: 130px;
+}
+</style>
