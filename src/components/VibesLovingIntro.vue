1a7ecb9409b30ff093e02e81c3887fcbeeaf0f8a
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-18 08:45:02 +0100

    create: VibesLovingIntro components

---
+++ src/components/VibesLovingIntro.vue
@@ -0,0 +1,12 @@
+<!-- componente della pagina what we offer -->
+<script>
+export default {
+    name: 'VibesLovingIntro'
+}
+</script>
+
+<template>
+    <h1>VibesLovingIntro</h1>
+</template>
+
+<style lang="scss" scoped></style>
